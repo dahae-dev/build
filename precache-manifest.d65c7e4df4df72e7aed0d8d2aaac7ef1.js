33099832a2cf32c854b8473006682150aace6909
dahaekim88 <dhkim1014@gmail.com>
2019-04-21 15:17:23 +0900

    build file for deploy

---
+++ precache-manifest.d65c7e4df4df72e7aed0d8d2aaac7ef1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc34c95a8c3b1f8297f6",
+    "url": "/static/js/main.bfe7fe76.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fa395f4b223f055f7d55",
+    "url": "/static/js/2.92004b71.chunk.js"
+  },
+  {
+    "revision": "265a80d9ecc23022c4b2fcb402fbb623",
+    "url": "/index.html"
+  }
+];
